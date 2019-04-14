fc95617bd3c476cd232ee4f5768b538480f79437
rinkeshpatel22 <rinkeshpatel22@gmail.com>
2019-04-15 00:04:58 +0530

    Print server url on console
    Print server url on console

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('Hello World'));
-app.listen(3000, () => {console.log('port 3000 working')});
+app.listen(3000, () => {console.log('Server running at http://127.0.0.1:3000/')});
